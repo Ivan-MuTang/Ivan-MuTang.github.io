a1ab784e679c236b9db49b7d597cd56fc69bce30
Ivan-MuTang <zhouganchun@gmail.com>
2024-09-19 17:41:59 +0800

    Site updated: 2024-09-19 17:41:57

--- source/_posts/Scripts/get_url.js
+++ source/_posts/Scripts/get_url.js
@@ -1,6 +1,16 @@
 function generateTimestampUrl() {
-  var timestamp = Math.round(new Date() / 1000);
-  var url = timestamp.toString(36)
+  // 获取当前时间戳（秒级）
+  var timestamp = Math.floor(Date.now() / 1000);
+  
+  // 将时间戳转换为36进制
+  var url = timestamp.toString(36);
+  
+  // 确保生成的字符串至少有6位，不足6位的前面补充 '0'
+  while (url.length < 6) {
+    url = '0' + url;
+  }
+  
   return url;
 }
+
 module.exports = generateTimestampUrl;
